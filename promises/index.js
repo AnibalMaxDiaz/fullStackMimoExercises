04e7cc901325f5b398118f56fd88635748bc7921
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-09-28 20:49:33 -0500

    exercise solved

--- promises/index.js
+++ promises/index.js
@@ -1,6 +1,6 @@
 const deserts = {
   chessCake : {
-    amount: 20,
+    amount: 10,
     price: 10,
   },
   iceCream : {
@@ -8,3 +8,40 @@ const deserts = {
     price: 5,      
   }
 };
+
+const budget = 20;
+function isAvailable() {
+  return new Promise((resolve, reject) => {
+    if (deserts.chessCake.amount > 0 ) {
+      resolve(true);
+    } else {
+      reject(false);
+    }
+  });
+}
+function checkPrice(){
+  return new Promise((resolve, reject)=> {
+    if (deserts.chessCake.price <= budget){
+      resolve("Purchasing desert..");
+    } else {
+      reject("Budget is not enough, canceling the order...");
+    }
+  })
+}
+function handleIsAvailable(isAvailable) {
+  return checkPrice(isAvailable);
+}
+function handlePrice(successValue) {
+  console.log(successValue)
+}
+const checkAvailability = isAvailable();
+checkAvailability
+  .then(handleIsAvailable)
+  .then(handlePrice)
+  .catch((rejectionValue) => {
+    console.log(rejectionValue);
+  });
+
+// Write your code here
+
+

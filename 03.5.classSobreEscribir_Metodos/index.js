a154154e30e3e1779aa3f1bd59ee566ee79d9623
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-10-09 18:53:24 -0500

    create example of renaming a mettod in a class

---
+++ 03.5.classSobreEscribir_Metodos/index.js
@@ -0,0 +1,26 @@
+
+class Human {
+  constructor(weapon) {
+    this.weapon = weapon;
+    this.health = 100;
+    this.damegeInflicted = 0;
+  }
+  attack() {
+    this.damegeInflicted += 10;
+    console.log(`Attacking with ${this.weapon} that inflicts ${this.damegeInflicted} points of damage`); 
+  }
+}
+class Wizard extends Human {
+  attack(){
+    this.health += 10;
+    super.attack();
+    this.damegeInflicted += 20;
+    console.log('Abra Kadabra, the wizard is attacking! with ' + this.weapon + ' that inflicts ' + this.damegeInflicted + ' points of damage' );
+  }
+}
+const aragorn = new Human('Sword');
+const gandalf = new Wizard('Staff');
+aragorn.attack();
+gandalf.attack();
+console.log(aragorn);
+console.log(gandalf);
